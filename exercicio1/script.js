23aa30b85a3bd303f370078ebec1473bd599c47f
Débora Lucia Koppe Soares <deboraluciak@hotmail.com>
2023-05-29 13:25:44 -0300

    revisando

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,16 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+let x = 1
+
+for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < 5; j++){
+
+        console.log(`Jogador ${x} - ${array[i]}`);
+
+        
+    }
+	
+    x++
+
+}
---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+const num = prompt("Digite um numero:");
+
+for(let i = 1; i <= 10; i++){
+	
+	let tabuada = num * i;
+    console.log(tabuada)
+
+}
